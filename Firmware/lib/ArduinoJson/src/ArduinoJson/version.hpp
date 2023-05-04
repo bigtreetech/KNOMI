a2437616be41327d85d67fe248d6f65d2d0c9c71
Alan.Ma <alansayyeah@gmail.com>
2023-05-04 19:23:40 +0800

    remove remote dependency library and change to local library

---
+++ Firmware/lib/ArduinoJson/src/ArduinoJson/version.hpp
@@ -0,0 +1,10 @@
+// ArduinoJson - https://arduinojson.org
+// Copyright © 2014-2022, Benoit BLANCHON
+// MIT License
+
+#pragma once
+
+#define ARDUINOJSON_VERSION "6.19.4"
+#define ARDUINOJSON_VERSION_MAJOR 6
+#define ARDUINOJSON_VERSION_MINOR 19
+#define ARDUINOJSON_VERSION_REVISION 4
