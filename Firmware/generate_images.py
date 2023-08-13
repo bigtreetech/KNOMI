e93c8e189f1997c43bedd548fd0ce87b65cc0b7d
Kirill Orlov <i@kirillorlov.pro>
2023-08-14 01:54:25 +0200

    converted part of images to generated from gif

--- Firmware/generate_images.py
+++ Firmware/generate_images.py
@@ -11,7 +11,16 @@ except ImportError:
     env.Execute("$PYTHONEXE -m pip install Pillow~=9.3.0")
     from lv_img_converter import Converter
 
-def conv_one_file(filepath, filename, out_path):
+gifs = [f for f in listdir('GIFS') if isfile(join('GIFS', f))]
+
+makedirs(join("src", "generated"), exist_ok=True)
+
+for gif in gifs:
+    parts = splitext(gif)
+    print("Converting: %s" % parts[0])
+    filepath = "GIFS/%s%s" % (parts[0],parts[1])
+    filename = parts[0]
+    out_path = "src/generated/%s.c" % parts[0]
     conv = Converter(filepath, filename, False, Converter.FLAG.CF_RAW_CHROMA)
 
     conv.convert(Converter.FLAG.CF_RAW_CHROMA, 0)
@@ -20,13 +29,3 @@ def conv_one_file(filepath, filename, out_path):
     with open(out_path, "w") as fi:
         res = conv.get_c_code_file(-1, c_arr)
         fi.write(res)
-    return "SUCCESS"
-
-gifs = [f for f in listdir('GIFS') if isfile(join('GIFS', f))]
-
-makedirs(join("src", "generated"), exist_ok=True)
-
-for gif in gifs:
-    parts = splitext(gif)
-    print("Converting: %s" % parts[0])
-    conv_one_file("GIFS/%s%s" % (parts[0],parts[1]), parts[0], "src/generated/%s.c" % parts[0])
