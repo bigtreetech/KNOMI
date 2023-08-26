9ed2427015519a0011ce631f028b569e2bbf1dac
Kirill Orlov <i@kirillorlov.pro>
2023-08-26 23:43:58 +0200

    ElegantOTA cleanup and autoformat

--- Firmware/src/ota/ElegantOTA.h
+++ Firmware/src/ota/ElegantOTA.h
@@ -1,24 +1,10 @@
-#ifndef ElegantOTA_h
-#define ElegantOTA_h
-
+#pragma once
 #include "Arduino.h"
-#include "stdlib_noniso.h"
-#include "elegantWebpage.h"
-
-#if defined(ESP8266)
-    #define HARDWARE "ESP8266"
-    #include "ESP8266WiFi.h"
-    #include "WiFiClient.h"
-    #include "ESP8266WebServer.h"
-    #include "ESP8266HTTPUpdateServer.h"
-#elif defined(ESP32)
-    #define HARDWARE "ESP32"
-    #include "WiFi.h"
-    #include "WiFiClient.h"
-    #include "WebServer.h"
-    #include "Update.h"
-#endif
-
+#include "WiFi.h"
+#include "WiFiClient.h"
+#include "WebServer.h"
+#include "Update.h"
+#include "../generated/elegantWebpage.h"
 
 class ElegantOtaClass{
   public:
@@ -28,20 +14,10 @@ class ElegantOtaClass{
     void onOTAStart(void callable(void));
     void onOTAProgress(void callable(void));
     void onOTAEnd(void callable(void));
-    #if defined(ESP8266)      
-      void begin(ESP8266WebServer *server, const char * username = "", const char * password = "");
-    #elif defined(ESP32)
-      void begin(WebServer *server, const char * username = "", const char * password = "");
-    #endif
-    
+    void begin(WebServer *server, const char * username = "", const char * password = "");
+
   private:
-    #if defined(ESP8266)
-        ESP8266WebServer *_server;
-        ESP8266HTTPUpdateServer _httpUpdater;
-    #endif
-    #if defined(ESP32)
-        WebServer *_server;
-    #endif
+    WebServer *_server;
 
     char _username[64];
     char _password[64];
@@ -56,4 +32,3 @@ class ElegantOtaClass{
 };
 
 extern ElegantOtaClass ElegantOTA;
-#endif
--- Firmware/ui/compress.js
+++ Firmware/ui/compress.js
@@ -12,13 +12,12 @@ const HTML = `
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
-    <link rel=icon href=/favicon.ico> <title>ElegantOTA</title>
-    <script data-name="BMC-Widget" async src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="6QGVpSj" data-description="Support me on Buy me a coffee!" data-message="You can always support my work by buying me a coffee!" data-color="#FF813F" data-position="right" data-x_margin="24" data-y_margin="24"></script>
+    <title>KNOMI</title>
 </head>
 
 <body style="overflow: hidden;">
     <noscript>
-        <strong>We're sorry but ElegantOTA doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+        <strong>We're sorry but KNOMI doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
     </noscript>
     <div id=app></div>
     <script defer>${BUNDLE_JS}</script>
@@ -56,15 +55,12 @@ gzip(HTML, { numiterations: 15 }, (err, output) => {
     return console.error(err);
   }
 
-  const FILE = `#ifndef ElegantOTAWebpage_h
-#define ElegantOTAWebpage_h
+  const FILE = `#pragma once
 
 const uint32_t ELEGANT_HTML_SIZE = ${output.length};
 const uint8_t ELEGANT_HTML[] PROGMEM = { 
 ${addLineBreaks(output)} 
 };
-
-#endif
 `;
 
   FS.writeFileSync(path.resolve(__dirname, '../src/generated/elegantWebpage.h'), FILE);
