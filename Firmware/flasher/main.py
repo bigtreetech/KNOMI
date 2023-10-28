3a91ffac31b2440b9aeb5228c22cc2d5822d1d60
Kirill Orlov <i@kirillorlov.pro>
2023-10-28 19:16:37 +0200

    attempt to fix clash between osx and linux

--- Firmware/flasher/main.py
+++ Firmware/flasher/main.py
@@ -2,17 +2,20 @@ import os
 
 import PyInstaller.__main__
 import esptool
+import platform
 
 jsons = os.path.join(os.path.dirname(esptool.__file__), "targets", "stub_flasher")
 
 
 PyInstaller.__main__.run([
     '--onefile',
+    '--noupx',
     '--specpath', '../.build/',
     '--workpath', '../.build/work',
     '--distpath', '../.build/dist',
     '--add-binary', '../.pio/build/esp32dev/firmware_full.bin:.',
     '--add-binary', '../.pio/build/esp32dev/buildinfo.json:.',
     '--add-data', jsons + ':esptool/targets/stub_flasher',
+    '-n', 'flash_firmware.' + platform.system(),
     'flash_firmware.py'
 ])
