06ba9cc4324515732cc42b74d9923eba0543bb08
Kirill Orlov <i@kirillorlov.pro>
2023-11-04 01:45:12 +0100

    better logging

--- Firmware/src/log.h
+++ Firmware/src/log.h
@@ -9,5 +9,5 @@ void logToSerial(const char *logLevel, const char *file, int line, const char *f
 #define LV_LOG_DEBUG(...) {}
 #endif
 
-#define LV_LOG_INFO(...) logToSerial("INFO", __FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LV_LOG_WARN(...) logToSerial("WARN", __FILE__, __LINE__, __func__, __VA_ARGS__)
+#define LV_LOG_INFO(...) logToSerial("INFO", __FILE__, __LINE__, __PRETTY_FUNCTION__ , __VA_ARGS__)
+#define LV_LOG_WARN(...) logToSerial("WARN", __FILE__, __LINE__, __PRETTY_FUNCTION__ , __VA_ARGS__)
