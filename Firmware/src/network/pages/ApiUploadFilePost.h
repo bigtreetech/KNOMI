25d0c9a062e1ddf7b27f7bd9919fbac1bdd8476f
Kirill Orlov <i@kirillorlov.pro>
2023-11-19 02:23:25 +0100

    file upload works

--- Firmware/src/network/pages/ApiUploadFilePost.h
+++ Firmware/src/network/pages/ApiUploadFilePost.h
@@ -14,72 +14,66 @@ public:
   }
 
   esp_err_t handler(httpd_req_t *req) override {
-    String header = getHeader(req, "Content-Type");
-
-    if (!header.startsWith("multipart/form-data")) {
-      httpd_resp_send_500(req);
-      return ESP_OK;
-    }
-
-    String boundary = "--" + header.substring(header.indexOf("boundary=") + 9);
     String filename;
     String size;
     auto builder = MD5Builder();
     builder.begin();
 
-    String errorCode = "200 OK";
+    String errorCode = HTTPD_200;
     String errorText = "OK";
 
     fs::File tempFile;
 
-    streamReadMultipart(req, boundary, [&](const String &formData) {
-      if (formData.equals("filename")) {
-        return readString(&filename);
-      }
-      if (formData.equals("size")) {
-        return readString(&size);
-      }
-      if (formData.equals("file")) {
-        auto currentFile = LittleFS.open("/" + filename, "r");
-        int available = LittleFS.totalBytes() - LittleFS.usedBytes() + currentFile.size();
-        currentFile.close();
+    if (!streamReadMultipart(req, [&](const String &formData, const String &fn) {
+          if (formData.equals("filename")) {
+            return readString(&filename);
+          }
+          if (formData.equals("size")) {
+            return readString(&size);
+          }
+          if (formData.equals("file")) {
+            auto currentFile = LittleFS.open("/" + filename, "r");
+            size_t available = LittleFS.totalBytes() - LittleFS.usedBytes() + currentFile.size();
+            currentFile.close();
 
-        int minimalAvailableSize = 8192;
+            int minimalAvailableSize = 8192;
 
-        int uploadSize = size.toInt();
+            int uploadSize = size.toInt();
 
-        if (available < minimalAvailableSize + uploadSize) {
-          errorCode = "500 Internal Server Error";
-          errorText = "Not enough of free space";
+            if (available < minimalAvailableSize + uploadSize) {
+              errorCode = HTTPD_500;
+              errorText = "Not enough of free space";
+              return (ReadCallback) nullptr;
+            }
+
+            updateProgress->total = uploadSize;
+            updateProgress->current = 0;
+            updateProgress->isInProgress = true;
+            LV_LOG_INFO("UpdateProgress %i / %i", updateProgress->current, updateProgress->total);
+            if (!updateProgress->waitForCanWrite()) {
+              errorCode = HTTPD_500;
+              errorText = "Internal error.";
+              return (ReadCallback) nullptr;
+            }
+            LV_LOG_INFO("UpdateProgress check done");
+
+            tempFile = LittleFS.open("/" + filename, "w");
+            LV_LOG_INFO("Upload start %s", filename.c_str());
+
+            ReadCallback md5print = [&](const char *buf, int idx, int size) {
+              updateProgress->current += size;
+              tempFile.write(((uint8_t *)buf) + idx, size);
+              LV_LOG_INFO("Written %i", size);
+
+              builder.add(((uint8_t *)buf) + idx, size);
+            };
+            return md5print;
+          }
+
           return (ReadCallback) nullptr;
-        }
-
-        updateProgress->total = uploadSize;
-        updateProgress->current = 0;
-        updateProgress->isInProgress = true;
-        LV_LOG_INFO("UpdateProgress %i / %i", updateProgress->current, updateProgress->total);
-        if (!updateProgress->waitForCanWrite()) {
-          errorCode = "500 Internal Server Error";
-          errorText = "Internal error.";
-          return (ReadCallback) nullptr;
-        }
-        LV_LOG_INFO("UpdateProgress check done");
-
-        tempFile = LittleFS.open("/" + filename, "w");
-        LV_LOG_INFO("Upload start %s", filename.c_str());
-
-        ReadCallback md5print = [&](const char *buf, int idx, int size) {
-          updateProgress->current += size;
-          tempFile.write(((uint8_t *)buf) + idx, size);
-          LV_LOG_INFO("Written %i", size);
-
-          builder.add(((uint8_t *)buf) + idx, size);
-        };
-        return md5print;
-      }
-
-      return (ReadCallback) nullptr;
-    });
+        })) {
+      return ESP_OK;
+    }
     builder.calculate();
     LV_LOG_INFO(filename.c_str());
     LV_LOG_INFO(size.c_str());
