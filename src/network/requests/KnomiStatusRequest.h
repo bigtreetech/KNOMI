b9d0ffbed6dd607bcecd7917efb419b347d29ca6
Kirill Orlov <me@kirillorlov.pro>
2024-02-17 18:29:00 +0100

    working qgling gif

--- src/network/requests/KnomiStatusRequest.h
+++ src/network/requests/KnomiStatusRequest.h
@@ -6,14 +6,14 @@ class KnomiStatusRequest : public KlipperApiRequest {
 
   void processJson(JsonDocument &doc) override {
 
-    bool isHoming = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["homing"].as<bool>();
-    bool isProbing = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["probing"].as<bool>();
-    bool isQgling = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["qgling"].as<bool>();
-    bool heatingNozzle = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["heating_nozzle"].as<bool>();
-    bool heatingBed = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["heating_bed"].as<bool>();
+    isHoming = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["homing"].as<bool>();
+    isProbing = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["probing"].as<bool>();
+    isQgling = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["qgling"].as<bool>();
+    isHeatingNozzle = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["heating_nozzle"].as<bool>();
+    isHeatingBed = doc["result"]["status"]["gcode_macro _KNOMI_STATUS"]["heating_bed"].as<bool>();
 
     LV_LOG_INFO("Knomi status: isHoming: %d, isProbing: %d, isQgling: %d, heatingNozzle: %d, heatingBed: %d", isHoming,
-                isProbing, isQgling, heatingNozzle, heatingBed);
+                isProbing, isQgling, isHeatingNozzle, isHeatingBed);
   }
 
 public:
