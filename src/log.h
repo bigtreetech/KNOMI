eda3ffb539eb71a059b762b29a8de96b1e0dee23
Kirill Orlov <me@kirillorlov.pro>
2024-02-25 21:04:12 +0100

    disabled debug logs

--- src/log.h
+++ src/log.h
@@ -10,6 +10,8 @@ void logToSerial(const char *logLevel, const char *file, int line, const char *f
 #define LV_LOG_HTTP(...) LV_LOG_IGNORE(__VA_ARGS__)
 #define LV_LOG_RESOURCEIMAGE(...) LV_LOG_IGNORE(__VA_ARGS__)
 
-#define LV_LOG_DEBUG(...) logToSerial("DEBUG", __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+#define LV_LOG_DEBUG(...)                                                                                              \
+  LV_LOG_IGNORE(__VA_ARGS__) // logToSerial("DEBUG", __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
+
 #define LV_LOG_INFO(...) logToSerial("INFO", __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
 #define LV_LOG_WARN(...) logToSerial("WARN", __FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
